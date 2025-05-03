28ccd224ef0107a34fb0e647c491f5f0b5389fde
Soniya-Ingalagi123 <soniyaingalagi18@gmail.com>
2025-05-03 11:41:24 +0530

    save

--- utiol.java
+++ utiol.java
@@ -9,11 +9,12 @@ public class utiol{
 public static void main(String[]args)
 {
 ioo g =new ioo();
-g.name="Khushi";
+g.name="Soniya";
 g.age=1;
     
 System.out.println("Name"+g.name);
 System.out.println("Name"+g.age);
+System.out.println("kkkkkkk");
 
 
 }}
