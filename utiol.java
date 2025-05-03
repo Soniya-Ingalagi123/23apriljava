38e0862fd214f8f0479e727d591008dbb79e1870
Soniya-Ingalagi123 <soniyaingalagi18@gmail.com>
2025-05-03 11:13:52 +0530

    eeerre

---
+++ utiol.java
@@ -0,0 +1,15 @@
+
+
+class ioo
+{
+    String name;
+}
+public class utiol{
+public static void main(String[]args)
+{
+ioo g =new ioo();
+g.name="soniya";
+    
+System.out.println("Name"+g.name);
+
+}}
